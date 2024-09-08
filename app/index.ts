5378f8fff5f1252d7f2529a28c855906256e156a
Perpetual Meninwa <perpetualmeninwa@gmail.com>
2024-09-08 19:44:33 +0100

    Update index.ts

--- app/index.ts
+++ app/index.ts
@@ -18,7 +18,15 @@ app.use(logger('combined', { stream: rotatingFileStream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
+
+const corsOptions = {
+  origin: '*', // Adjust this to a specific domain or an array of domains if needed
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], // List the methods you want to allow
+  allowedHeaders: ['Content-Type', 'Authorization'], // List the headers you want to allow
+  credentials: true, // If you want to allow cookies to be sent with requests
+};
+
+app.use(cors(corsOptions));
 
 // Define routers
 app.use(authRouter)
